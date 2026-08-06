c348e651ccdc3dd952b22c38f22fbd06d5c1aa9a
SeldonHh <hadrien.heurteaux91@gmail.com>
2026-08-06 20:38:20 +0200

    updated player movement

--- roseshell-interstellar-garden/scenes/Singleton/Global.gd
+++ roseshell-interstellar-garden/scenes/Singleton/Global.gd
@@ -2,3 +2,4 @@ extends Node
 
 var player : Node2D
 var black_hole : Node2D
+var keyboard := false
--- roseshell-interstellar-garden/scenes/player/player.gd
+++ roseshell-interstellar-garden/scenes/player/player.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 var anchor : Vector2 = Vector2(.5,.5)
-var radius : float = 300
-@export var speed := 96.0
+var radius : float = 200
+@export var speed := .08
 @onready var rotation_factor := PI
 @onready var body: CharacterBody2D = $body
 
@@ -12,10 +12,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Global.black_hole:
 		position  = Global.black_hole.position
-	if Input.is_action_pressed("left"):
-		rotation_factor = lerp(rotation_factor,rotation_factor-speed*2*delta,delta)
-	if Input.is_action_pressed("right"):
-		rotation_factor = lerp(rotation_factor,rotation_factor+speed*2*delta,delta)
 	
-	var end = Vector2(0, radius).rotated(rotation_factor)
-	body.position = end
+	if Global.keyboard:
+		if Input.is_action_pressed("left"):
+			rotation_factor = lerp(rotation_factor,rotation_factor-speed*1000*delta,delta)
+		if Input.is_action_pressed("right"):
+			rotation_factor = lerp(rotation_factor,rotation_factor+speed*1000*delta,delta)
+	else:
+		var mouse_position = get_global_mouse_position()
+		var target_angle = get_angle_to(mouse_position)
+		rotation_factor = lerp_angle(rotation_factor,target_angle,speed)
+	body.position = Vector2(radius, 0).rotated(rotation_factor)
