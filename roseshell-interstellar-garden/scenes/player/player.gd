e7dbbff03589c80bc8febde4762aca9b7a47b54b
SeldonHh <hadrien.heurteaux91@gmail.com>
2026-08-06 22:35:11 +0200

    bip bap boup trail

--- roseshell-interstellar-garden/scenes/player/player.gd
+++ roseshell-interstellar-garden/scenes/player/player.gd
@@ -3,8 +3,11 @@ extends Node2D
 var anchor : Vector2 = Vector2(.5,.5)
 @export var radius : float = 120
 @export var speed := .08
+@export var previous_rotation := PI
 @onready var rotation_factor := PI
 @onready var body: CharacterBody2D = $body
+@onready var trail: Line2D = $body/trail
+@export var trail_lenght := 100.0
 
 func _ready() -> void:
 	Global.player = self
@@ -22,4 +25,12 @@ func _process(delta: float) -> void:
 		var mouse_position = get_global_mouse_position()
 		var target_angle = get_angle_to(mouse_position)
 		rotation_factor = lerp_angle(rotation_factor,target_angle,speed)
+	if previous_rotation <= rotation_factor-PI/100 or previous_rotation >= rotation_factor+PI/100:
+		trail.show()
+		trail_lenght = lerp(trail_lenght,(previous_rotation-rotation_factor)*1000,delta*16)
+	else:
+		trail.hide()
+	trail.points = [Vector2.ZERO,Vector2(0,trail_lenght)]
+	body.rotation = rotation_factor
 	body.position = Vector2(radius, 0).rotated(rotation_factor)
+	previous_rotation = rotation_factor
