81ae744b1b9bd66ff2200756e91c779dc77ce246
SeldonHh <hadrien.heurteaux91@gmail.com>
2026-08-08 13:01:24 +0200

    charted neru's song

--- roseshell-interstellar-garden/resources/SongChart.gd
+++ roseshell-interstellar-garden/resources/SongChart.gd
@@ -6,11 +6,11 @@ class_name SongChart
 
 @export_category("parameters")
 @export var song_file := preload("uid://tuhk64r4l4us")
-@export var decibel_reduction := -10
+@export var decibel_reduction := 10
 @export var start_time := 3.9
-@export var angles := [0, 180, 90, -90, 180]
+@export var angles : Array[int]= [0, 180, 90, -90, 180]
 	
-@export var intervals := [
+@export var intervals : Array[Dictionary]= [
 		{"until": 10.0, "interval": 1.333},
 		{"until": 31.0, "interval": 0.66},
 	]
