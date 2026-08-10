7634c19a43202d8e6b881485ee9296b2973b547e
Punduch <punduch5647@gmail.com>
2026-08-10 16:04:47 +0200

    Last Singularity charted... yay!!!

--- roseshell-interstellar-garden/scenes/IdleBreathScript.gd
+++ roseshell-interstellar-garden/scenes/IdleBreathScript.gd
@@ -1,25 +1,30 @@
 extends Node2D
 
-# 1 = Orange
-# 2 = Blue
-# 3 = Purple
-# 4 = Green
-# 5 = Red
-# 6 = Yellow
-# 7 = Cyan
-# 8 = Pink
-# 9 = Lime
-# 10 = White
-# 11 = More blue
-
 @export var breathe_speed := 1.2
 @export var breathe_amount := 0.015
 @export var float_amount := 3.0
+
+const COLOR_TRANSITION_TIME := 3.3
 
 var origin_position: Vector2
 var origin_scale: Vector2
 var t := 0.0
 var blackhole_index := 1
+
+var chart_data = null
+var audio_player: AudioStreamPlayer = null
+var current_asteroid_type := ""
+
+var transition_progress := 1.0
+var transition_start_a := Color.WHITE
+var transition_start_b := Color.WHITE
+var transition_start_comet := Color.WHITE
+
+var transition_target_a := Color.WHITE
+var transition_target_b := Color.WHITE
+var transition_target_comet := Color.WHITE
+
+var color_tween: Tween = null
 
 @onready var disk: GPUParticles2D = $Disk
 @onready var glow0: Sprite2D = $Glow0
@@ -115,71 +120,389 @@ var variants = {
 		"breathe_speed": 1.35,
 		"breathe_amount": 0.018,
 		"comet_color": Color("007cffff")
-	},
+	}
 }
 
 func _ready() -> void:
 	Global.black_hole = self
+
 	origin_position = get_viewport().get_visible_rect().size / 2.0
 	origin_scale = scale
+
+	audio_player = find_music_player()
+	load_chart_data()
+
 	apply_blackhole_index(blackhole_index)
 
 func _process(delta: float) -> void:
 	origin_position = get_viewport().get_visible_rect().size / 2.0
+
 	t += delta
+
 	var breath = sin(t * breathe_speed)
 	var sway_x = sin(t * breathe_speed * 0.6) * float_amount
 	var sway_y = cos(t * breathe_speed * 0.8) * float_amount
+
 	scale = origin_scale * (1.0 + breath * breathe_amount)
 	position = origin_position + Vector2(sway_x, sway_y)
+
+	if blackhole_index == 30:
+		update_chart_color()
+
+	if color_tween != null and color_tween.is_valid():
+		update_color_transition()
+
+func find_music_player() -> AudioStreamPlayer:
+	var music_node = get_tree().current_scene.find_child("Music", true, false)
+
+	if music_node is AudioStreamPlayer:
+		return music_node
+
+	var audio_players = get_tree().get_nodes_in_group("music")
+
+	for node in audio_players:
+		if node is AudioStreamPlayer:
+			return node
+
+	return null
+
+func load_chart_data() -> void:
+	var chart_path = "res://Charts/song_chart.tres"
+
+	if ResourceLoader.exists(chart_path):
+		chart_data = load(chart_path)
+	else:
+		setup_fallback_data()
+
+func setup_fallback_data() -> void:
+	chart_data = {
+		"intervals": [
+			{"until": 22.2},
+			{"until": 66.0},
+			{"until": 90.0},
+			{"until": 121.0},
+			{"until": 138.0},
+			{"until": 189.0},
+			{"until": 200.0},
+			{"until": 221.0},
+			{"until": 244.0},
+			{"until": 254.0},
+			{"until": 266.0},
+			{"until": 288.0},
+			{"until": 300.0},
+			{"until": 310.0},
+			{"until": 322.0},
+			{"until": 330.0}
+		],
+		"asteroid_types": [
+			"Green",
+			"Regular",
+			"Yellow",
+			"Regular",
+			"Regular",
+			"Green",
+			"Yellow",
+			"Red",
+			"Red",
+			"Regular",
+			"Yellow",
+			"Green",
+			"Green",
+			"Regular",
+			"Yellow",
+			"Red"
+		]
+	}
+
+func get_asteroid_type_at_time(time: float) -> String:
+	if chart_data == null:
+		return "Regular"
+
+	var intervals = chart_data.get("intervals", [])
+	var asteroid_types = chart_data.get("asteroid_types", [])
+
+	if intervals.is_empty() or asteroid_types.is_empty():
+		return "Regular"
+
+	var count = min(intervals.size(), asteroid_types.size())
+
+	for i in range(count):
+		var until = intervals[i].get("until", 0.0)
+
+		if time <= until:
+			return asteroid_types[i]
+
+	return asteroid_types[count - 1]
+
+func update_chart_color() -> void:
+	if audio_player == null:
+		return
+
+	if not audio_player.playing:
+		return
+
+	var elapsed := audio_player.get_playback_position()
+	var new_type := get_asteroid_type_at_time(elapsed)
+
+	if new_type == current_asteroid_type:
+		return
+
+	current_asteroid_type = new_type
+
+	var colors = get_chart_colors(new_type)
+
+	start_color_transition(
+		colors["color_a"],
+		colors["color_b"],
+		colors["comet_color"]
+	)
+
+func get_chart_colors(asteroid_type: String) -> Dictionary:
+	match asteroid_type:
+		"Green":
+			return {
+				"color_a": Color("00d02dff"),
+				"color_b": Color("7dff5fff"),
+				"comet_color": Color("42d047ff")
+			}
+
+		"Yellow":
+			return {
+				"color_a": Color("#ffe600"),
+				"color_b": Color("#fff59d"),
+				"comet_color": Color("#ffe600")
+			}
+
+		"Red":
+			return {
+				"color_a": Color("#ff1744"),
+				"color_b": Color("#ff758f"),
+				"comet_color": Color("#ff1744")
+			}
+
+		_:
+			return {
+				"color_a": Color("#ff6a00"),
+				"color_b": Color("#ffb000"),
+				"comet_color": Color("#ff6a00")
+			}
 
 func set_blackhole_index(index) -> void:
 	blackhole_index = int(index)
 	apply_blackhole_index(blackhole_index)
 
 func apply_blackhole_index(index: int) -> void:
+	if index == 30:
+		apply_chart_blackhole()
+		return
+
 	if not variants.has(index):
 		index = 1
+
+	if color_tween != null and color_tween.is_valid():
+		color_tween.kill()
+
 	var variant = variants[index]
+
 	breathe_speed = variant["breathe_speed"]
 	breathe_amount = variant["breathe_amount"]
+
 	scale = origin_scale * variant["size"]
-	
+
+	var color_a: Color = variant["color_a"]
+	var color_b: Color = variant["color_b"]
+	var comet_color: Color = variant["comet_color"]
+
+	transition_start_a = color_a
+	transition_start_b = color_b
+	transition_start_comet = comet_color
+
+	transition_target_a = color_a
+	transition_target_b = color_b
+	transition_target_comet = comet_color
+
+	transition_progress = 1.0
+
+	apply_disk_colors(color_a, color_b)
+	update_glow_colors(color_a)
+	update_comet_colors(comet_color)
+
+func apply_chart_blackhole() -> void:
+	var regular_variant = variants[1]
+
+	breathe_speed = regular_variant["breathe_speed"]
+	breathe_amount = regular_variant["breathe_amount"]
+
+	scale = origin_scale * regular_variant["size"]
+
+	current_asteroid_type = ""
+
+	var asteroid_type := "Regular"
+
+	if audio_player != null and audio_player.playing:
+		asteroid_type = get_asteroid_type_at_time(
+			audio_player.get_playback_position()
+		)
+
+	current_asteroid_type = asteroid_type
+
+	var colors = get_chart_colors(asteroid_type)
+
+	transition_start_a = colors["color_a"]
+	transition_start_b = colors["color_b"]
+	transition_start_comet = colors["comet_color"]
+
+	transition_target_a = colors["color_a"]
+	transition_target_b = colors["color_b"]
+	transition_target_comet = colors["comet_color"]
+
+	transition_progress = 1.0
+
+	apply_disk_colors(
+		colors["color_a"],
+		colors["color_b"]
+	)
+
+	update_glow_colors(colors["color_a"])
+	update_comet_colors(colors["comet_color"])
+
+func start_color_transition(
+	new_color_a: Color,
+	new_color_b: Color,
+	new_comet_color: Color
+) -> void:
+	if color_tween != null and color_tween.is_valid():
+		var current_a = transition_start_a.lerp(
+			transition_target_a,
+			transition_progress
+		)
+
+		var current_b = transition_start_b.lerp(
+			transition_target_b,
+			transition_progress
+		)
+
+		var current_comet = transition_start_comet.lerp(
+			transition_target_comet,
+			transition_progress
+		)
+
+		transition_start_a = current_a
+		transition_start_b = current_b
+		transition_start_comet = current_comet
+
+		color_tween.kill()
+	else:
+		transition_start_a = transition_target_a
+		transition_start_b = transition_target_b
+		transition_start_comet = transition_target_comet
+
+	transition_target_a = new_color_a
+	transition_target_b = new_color_b
+	transition_target_comet = new_comet_color
+
+	transition_progress = 0.0
+
+	color_tween = create_tween()
+	color_tween.set_trans(Tween.TRANS_SINE)
+	color_tween.set_ease(Tween.EASE_IN_OUT)
+
+	color_tween.tween_property(
+		self,
+		"transition_progress",
+		1.0,
+		COLOR_TRANSITION_TIME
+	)
+
+func update_color_transition() -> void:
+	var color_a = transition_start_a.lerp(
+		transition_target_a,
+		transition_progress
+	)
+
+	var color_b = transition_start_b.lerp(
+		transition_target_b,
+		transition_progress
+	)
+
+	var comet_color = transition_start_comet.lerp(
+		transition_target_comet,
+		transition_progress
+	)
+
+	apply_disk_colors(color_a, color_b)
+	update_glow_colors(color_a)
+	update_comet_colors(comet_color)
+
+func apply_disk_colors(color_a: Color, color_b: Color) -> void:
 	var material = disk.process_material
-	if material:
-		material = material.duplicate()
-		disk.process_material = material
-		var gradient = Gradient.new()
-		gradient.colors = PackedColorArray([
-			Color(variant["color_a"], 0.0),
-			variant["color_a"],
-			variant["color_b"],
-			Color(variant["color_b"], 0.0)
-		])
-		gradient.offsets = PackedFloat32Array([0.0, 0.3, 0.7, 1.0])
-		var gradient_texture = GradientTexture1D.new()
-		gradient_texture.gradient = gradient
-		material.color_ramp = gradient_texture
-	
-	update_glow_colors(variant["color_a"])
-	update_comet_colors(variant["comet_color"])
+
+	if material == null:
+		return
+
+	material = material.duplicate()
+
+	var gradient = Gradient.new()
+
+	gradient.colors = PackedColorArray([
+		Color(color_a, 0.0),
+		color_a,
+		color_b,
+		Color(color_b, 0.0)
+	])
+
+	gradient.offsets = PackedFloat32Array([
+		0.0,
+		0.3,
+		0.7,
+		1.0
+	])
+
+	var gradient_texture = GradientTexture1D.new()
+	gradient_texture.gradient = gradient
+
+	material.color_ramp = gradient_texture
+	disk.process_material = material
 
 func update_glow_colors(base_color: Color) -> void:
 	if glow0:
-		glow0.modulate = Color(base_color.r, base_color.g, base_color.b, 0.8)
+		glow0.modulate = Color(
+			base_color.r,
+			base_color.g,
+			base_color.b,
+			0.8
+		)
+
 	if glow1:
-		glow1.modulate = Color(base_color.r, base_color.g, base_color.b, 0.6)
+		glow1.modulate = Color(
+			base_color.r,
+			base_color.g,
+			base_color.b,
+			0.6
+		)
+
 	if glow2:
-		glow2.modulate = Color(base_color.r, base_color.g, base_color.b, 0.4)
+		glow2.modulate = Color(
+			base_color.r,
+			base_color.g,
+			base_color.b,
+			0.4
+		)
+
 	if glow3:
-		glow3.modulate = Color(base_color.r, base_color.g, base_color.b, 0.2)
+		glow3.modulate = Color(
+			base_color.r,
+			base_color.g,
+			base_color.b,
+			0.2
+		)
 
 func update_comet_colors(comet_color: Color) -> void:
 	var comets = get_tree().get_nodes_in_group("comet")
+
 	for comet in comets:
 		if comet.has_method("set_comet_color"):
 			comet.set_comet_color(comet_color)
 
-
 func _on_neru_1_pressed() -> void:
-	pass # Replace with function body.
+	pass
--- roseshell-interstellar-garden/scenes/dust_fogs.gd
+++ roseshell-interstellar-garden/scenes/dust_fogs.gd
@@ -1,28 +1,285 @@
 extends Node2D
 
-var dust_fog_index : int = 0
-var transparency_multiplier : float = 0.3
+var dust_fog_index: int = 0
+var transparency_multiplier: float = 0.3
+var chart_data = null
+var current_asteroid_type: String = ""
+var is_dust_index_10: bool = false
+var audio_player: AudioStreamPlayer = null
+
+var color_tween: Tween = null
+var active_gradient: Gradient = null
+var target_colors: Array[Color] = []
+var transition_progress: float = 1.0
+
+const COLOR_TRANSITION_TIME := 3.3
 
 @onready var fog_particles = $FogParticles
 
+func _ready():
+	load_chart_data()
+	audio_player = find_music_player()
+
+func _process(_delta):
+	if color_tween == null or not color_tween.is_valid():
+		return
+
+	if active_gradient == null or target_colors.is_empty():
+		return
+
+	var current_colors = active_gradient.colors
+
+	for i in range(min(current_colors.size(), target_colors.size())):
+		var start_color = current_colors[i]
+		var target_color = target_colors[i]
+		active_gradient.set_color(
+			i,
+			start_color.lerp(target_color, transition_progress)
+		)
+
+func _physics_process(_delta):
+	if not is_dust_index_10:
+		return
+
+	if chart_data == null:
+		return
+
+	if audio_player == null:
+		return
+
+	if not audio_player.playing:
+		return
+
+	var elapsed := audio_player.get_playback_position()
+	var new_type := get_asteroid_type_at_time(elapsed)
+
+	if new_type != current_asteroid_type:
+		current_asteroid_type = new_type
+		apply_asteroid_color(new_type, true)
+
+func find_music_player() -> AudioStreamPlayer:
+	var music_node = get_tree().current_scene.find_child("Music", true, false)
+
+	if music_node is AudioStreamPlayer:
+		return music_node
+
+	var audio_players = get_tree().get_nodes_in_group("music")
+
+	for node in audio_players:
+		if node is AudioStreamPlayer:
+			return node
+
+	return null
+
 func set_dust_fog_index(value: int):
 	dust_fog_index = value
-	if dust_fog_index >= 1 and dust_fog_index <= 5:
+	is_dust_index_10 = (dust_fog_index == 10)
+
+	if (dust_fog_index >= 1 and dust_fog_index <= 5) or is_dust_index_10:
 		fog_particles.visible = true
 		fog_particles.emitting = true
-		apply_color_scheme(dust_fog_index)
+
+		if is_dust_index_10:
+			current_asteroid_type = ""
+
+			if audio_player != null and audio_player.playing:
+				var elapsed := audio_player.get_playback_position()
+				var asteroid_type := get_asteroid_type_at_time(elapsed)
+
+				current_asteroid_type = asteroid_type
+				apply_asteroid_color(asteroid_type, false)
+			else:
+				apply_asteroid_color("Regular", false)
+		else:
+			current_asteroid_type = ""
+			apply_original_color(dust_fog_index)
 	else:
 		fog_particles.visible = false
 		fog_particles.emitting = false
 
-func apply_color_scheme(index: int):
+func load_chart_data():
+	setup_fallback_data()
+
+func setup_fallback_data():
+	chart_data = {
+		"intervals": [
+			{"until": 22.2},
+			{"until": 66.0},
+			{"until": 90.0},
+			{"until": 121.0},
+			{"until": 138.0},
+			{"until": 189.0},
+			{"until": 200.0},
+			{"until": 221.0},
+			{"until": 244.0},
+			{"until": 254.0},
+			{"until": 266.0},
+			{"until": 288.0},
+			{"until": 300.0},
+			{"until": 310.0},
+			{"until": 322.0},
+			{"until": 330.0}
+		],
+		"asteroid_types": [
+			"Regular",
+			"Regular",
+			"Yellow",
+			"Regular",
+			"Regular",
+			"Green",
+			"Yellow",
+			"Red",
+			"Red",
+			"Regular",
+			"Yellow",
+			"Green",
+			"Green",
+			"Regular",
+			"Yellow",
+			"Red"
+		]
+	}
+
+func get_asteroid_type_at_time(time: float) -> String:
+	if chart_data == null:
+		return "Regular"
+
+	var intervals = chart_data.get("intervals", [])
+	var asteroid_types = chart_data.get("asteroid_types", [])
+
+	if intervals.is_empty() or asteroid_types.is_empty():
+		return "Regular"
+
+	var count = min(intervals.size(), asteroid_types.size())
+
+	for i in range(count):
+		var until = intervals[i].get("until", 0.0)
+
+		if time <= until:
+			return asteroid_types[i]
+
+	return asteroid_types[count - 1]
+
+func get_asteroid_colors(asteroid_type: String) -> Array[Color]:
+	var alpha_mult = transparency_multiplier
+
+	match asteroid_type:
+		"Regular":
+			return [
+				Color(0.2, 0.05, 0.0, 0.0),
+				Color(1.0, 0.5, 0.0, 0.8 * alpha_mult),
+				Color(1.0, 0.7, 0.0, 1.0 * alpha_mult),
+				Color(0.8, 0.4, 0.0, 0.9 * alpha_mult),
+				Color(0.5, 0.2, 0.0, 0.6 * alpha_mult),
+				Color(0.2, 0.05, 0.0, 0.0)
+			]
+
+		"Green":
+			return [
+				Color(0.0, 0.2, 0.0, 0.0),
+				Color(0.0, 1.0, 0.0, 0.8 * alpha_mult),
+				Color(0.2, 1.0, 0.0, 1.0 * alpha_mult),
+				Color(0.0, 0.9, 0.0, 0.9 * alpha_mult),
+				Color(0.0, 0.6, 0.0, 0.6 * alpha_mult),
+				Color(0.0, 0.2, 0.0, 0.0)
+			]
+
+		"Yellow":
+			return [
+				Color(0.2, 0.15, 0.0, 0.0),
+				Color(1.0, 0.9, 0.0, 0.8 * alpha_mult),
+				Color(1.0, 1.0, 0.0, 1.0 * alpha_mult),
+				Color(0.8, 0.8, 0.0, 0.9 * alpha_mult),
+				Color(0.6, 0.6, 0.0, 0.6 * alpha_mult),
+				Color(0.2, 0.15, 0.0, 0.0)
+			]
+
+		"Red":
+			return [
+				Color(0.2, 0.0, 0.0, 0.0),
+				Color(0.9, 0.0, 0.0, 0.8 * alpha_mult),
+				Color(1.0, 0.0, 0.0, 1.0 * alpha_mult),
+				Color(0.8, 0.0, 0.0, 0.9 * alpha_mult),
+				Color(0.5, 0.0, 0.0, 0.6 * alpha_mult),
+				Color(0.2, 0.0, 0.0, 0.0)
+			]
+
+		_:
+			return [
+				Color(0.2, 0.05, 0.0, 0.0),
+				Color(1.0, 0.5, 0.0, 0.8 * alpha_mult),
+				Color(1.0, 0.7, 0.0, 1.0 * alpha_mult),
+				Color(0.8, 0.4, 0.0, 0.9 * alpha_mult),
+				Color(0.5, 0.2, 0.0, 0.6 * alpha_mult),
+				Color(0.2, 0.05, 0.0, 0.0)
+			]
+
+func apply_asteroid_color(asteroid_type: String, smooth: bool = true):
 	var proc_material = fog_particles.process_material
+
 	if proc_material == null:
 		return
-	
+
+	var colors = get_asteroid_colors(asteroid_type)
+
+	if active_gradient == null:
+		active_gradient = Gradient.new()
+		active_gradient.colors = colors
+		active_gradient.offsets = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+
+		var gradient_texture = GradientTexture1D.new()
+		gradient_texture.gradient = active_gradient
+
+		proc_material.color_ramp = gradient_texture
+		fog_particles.process_material = proc_material
+
+		target_colors = colors
+		transition_progress = 1.0
+		return
+
+	if not smooth:
+		if color_tween != null and color_tween.is_valid():
+			color_tween.kill()
+
+		active_gradient.colors = colors
+		target_colors = colors
+		transition_progress = 1.0
+		return
+
+	if color_tween != null and color_tween.is_valid():
+		color_tween.kill()
+
+	var starting_colors: Array[Color] = []
+
+	for color in active_gradient.colors:
+		starting_colors.append(color)
+
+	target_colors = colors
+	transition_progress = 0.0
+
+	color_tween = create_tween()
+	color_tween.set_trans(Tween.TRANS_SINE)
+	color_tween.set_ease(Tween.EASE_IN_OUT)
+	color_tween.tween_property(self, "transition_progress", 1.0, COLOR_TRANSITION_TIME)
+
+	color_tween.finished.connect(func():
+		if active_gradient != null:
+			active_gradient.colors = target_colors
+		transition_progress = 1.0
+	)
+
+func apply_original_color(index: int):
+	var proc_material = fog_particles.process_material
+
+	if proc_material == null:
+		return
+
+	if color_tween != null and color_tween.is_valid():
+		color_tween.kill()
+
 	var gradient = Gradient.new()
 	var alpha_mult = transparency_multiplier
-	
+
 	match index:
 		1:
 			gradient.colors = [
@@ -33,8 +290,7 @@ func apply_color_scheme(index: int):
 				Color(0.6, 0.8, 0.9, 0.6 * alpha_mult),
 				Color(0.0, 0.5, 0.8, 0.0)
 			]
-			gradient.offsets = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-			
+
 		2:
 			gradient.colors = [
 				Color(0.8, 0.0, 0.4, 0.0),
@@ -44,8 +300,7 @@ func apply_color_scheme(index: int):
 				Color(0.5, 0.0, 0.8, 0.6 * alpha_mult),
 				Color(0.3, 0.0, 0.6, 0.0)
 			]
-			gradient.offsets = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-			
+
 		3:
 			gradient.colors = [
 				Color(0.0, 0.3, 0.8, 0.0),
@@ -55,8 +310,7 @@ func apply_color_scheme(index: int):
 				Color(0.8, 0.9, 1.0, 0.6 * alpha_mult),
 				Color(0.9, 0.9, 1.0, 0.0)
 			]
-			gradient.offsets = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-			
+
 		4:
 			gradient.colors = [
 				Color(0.8, 0.2, 0.0, 0.0),
@@ -66,8 +320,7 @@ func apply_color_scheme(index: int):
 				Color(0.9, 0.8, 0.2, 0.6 * alpha_mult),
 				Color(0.8, 0.7, 0.1, 0.0)
 			]
-			gradient.offsets = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-			
+
 		5:
 			gradient.colors = [
 				Color(0.1, 0.0, 0.2, 0.0),
@@ -77,8 +330,13 @@ func apply_color_scheme(index: int):
 				Color(0.5, 0.0, 0.6, 0.6 * alpha_mult),
 				Color(0.2, 0.0, 0.3, 0.0)
 			]
-			gradient.offsets = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-	
+
+	gradient.offsets = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+
+	active_gradient = gradient
+
 	var gradient_texture = GradientTexture1D.new()
-	gradient_texture.gradient = gradient
+	gradient_texture.gradient = active_gradient
+
 	proc_material.color_ramp = gradient_texture
+	fog_particles.process_material = proc_material
