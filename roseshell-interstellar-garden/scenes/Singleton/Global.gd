64da8bfcd298dfc1776e93ecc656acb46d2a1e16
SeldonHh <hadrien.heurteaux91@gmail.com>
2026-08-10 11:37:03 +0200

    arrow and bug fix

--- roseshell-interstellar-garden/scenes/Singleton/Global.gd
+++ roseshell-interstellar-garden/scenes/Singleton/Global.gd
@@ -3,13 +3,13 @@ extends Node
 var player : Node2D
 var black_hole : Node2D
 var keyboard := false
-@onready var IS_DEBUG = "debug" in OS.get_cmdline_args() or true
+@onready var IS_DEBUG = "debug" in OS.get_cmdline_args()
 var main_menu : Control
 var sfx_volume := -8.5
 var music_volume := 1.0
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_action_just_pressed("pause") and false:
 		get_tree().paused = true
 		main_menu.show()
 		main_menu.show_settings()
